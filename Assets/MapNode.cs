e0cfc7a4443bdbe007e9239c4c81530a8a44cd91
James Shipp <jmscshipp202@gmail.com>
2021-07-03 17:54:23 -0400

    DANGEROUS COMMIT TO MAIN
    first one baby

---
+++ Assets/MapNode.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapNode : MonoBehaviour
+{
+    public MapNode northNode;
+    public MapNode eastNode;
+    public MapNode southNode;
+    public MapNode westNode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
